35d144b52bce3e9853175fa0f888a8d8aa253bcc
AmilcarArmmand <armmand@gmail.com>
2020-05-05 20:13:58 +0000

    Changed to print a negative digit if the number is negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 lastdigit = abs(number) % 10
-
+if number < 0:
+    lastdigit = -lastdigit
 print("Last digit of {} is {} ".format(number, lastdigit, end=""))
 if lastdigit > 5:
     print("and is greater than 5")
