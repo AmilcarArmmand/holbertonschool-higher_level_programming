8de1283189f404a8663db19134c495eee67c4f49
Amilcar Armmand <armmand@gmail.com>
2020-07-13 22:37:51 +0000

    added task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- script that creates a table called first_table in the current
+-- MySQL server, database name will be passed as argument of mysql command
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
