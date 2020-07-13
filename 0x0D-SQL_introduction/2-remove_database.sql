0c6a38b8eac79d0498332e1d9ccb7c1c5866f5ef
Amilcar Armmand <armmand@gmail.com>
2020-07-13 22:07:29 +0000

    updated comments in task 2

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,3 @@
--- mySQL list all database
+-- script that deletes the database hbtn_0c_0 from mySQL server
+-- if the database hbtn_0c_0 does not exist, the script should fail
 DROP DATABASE IF EXISTS hbtn_0c_0;
