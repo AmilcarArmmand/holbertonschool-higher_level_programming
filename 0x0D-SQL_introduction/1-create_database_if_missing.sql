46e1be041e811a662deb4297b85ebd8c54b31821
Amilcar Armmand <armmand@gmail.com>
2020-07-13 22:13:28 +0000

    Updated comments in task 1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,4 @@
--- mySQL list all database
+-- script creates the database hbtn_0c_0 in MySQL server
+-- if database hbtn_0c_0 already exist, script should not fail
+-- not allowed to use the SELECT of SHOW statements
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
