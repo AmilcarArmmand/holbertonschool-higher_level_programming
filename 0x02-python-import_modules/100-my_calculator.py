807ec01c1d2518cdd7194c613474b88715d70ac2
armmand@gmail.com <armmand@gmail.com>
2020-12-10 22:13:37 +0000

    Check if operator entered is in the list of operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,4 +7,9 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        
+        operations = {"+": add, "-": sub, "*": mul, "/": div}
+        if sys.argv[2] not in list(operations.keys()):
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+
+            
