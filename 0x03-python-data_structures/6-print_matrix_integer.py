9a9526d93aed6250049aa98e357cf2e4f5c0cfa0
AmilcarArmmand <armmand@gmail.com>
2020-12-16 03:05:36 +0000

    Now prints new line

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+        return
+    for row in matrix:
+        x = list(" " * (len(row) - 1))
+        x.append("\n")
+        y = zip(row, x)
+        for t in y:
+            print("{:d}{:c}".format(t[0], ord(t[1])), end="")
+            
