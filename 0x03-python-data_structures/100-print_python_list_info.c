8cc465c807b112a66360aa6e320f68f477c684bb
AmilcarArmmand <armmand@gmail.com>
2020-12-16 03:32:29 +0000

    Added base file for task 14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,14 @@
+#include <object.h>
+#include <listobject.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - function that prints CPython reference info
+ *
+ * @p: pointer to PyObject
+ * Return: No return value
+ */
+void print_python_list_info(PyObject *p)
+{
+
+}
