2177476ec70ee2c0c4835b92dca7535cb5068beb
AmilcarArmmand <armmand@gmail.com>
2021-01-05 21:59:06 +0000

    Added task 1 class and method

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Square: Class that defines a square
+"""
+
+
+class Square:
+    """ Class object """
+    def __init__(self, size):
+        """ Method to create a new instance of a class object
+
+        Args:
+            size: The size of the new square
+
+        """
+        self.__size = size
