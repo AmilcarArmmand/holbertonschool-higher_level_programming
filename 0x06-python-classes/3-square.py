591982e26db75c043f1fc47d423c64861a07f9b3
AmilcarArmmand <armmand@gmail.com>
2021-01-06 01:14:04 +0000

    Added task 3 method to return area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Square: Class that defines a square
+"""
+
+
+class Square:
+    """ Class object """
+    def __init__(self, size=0):
+        """ Method to create a new instance of a class object
+
+        Args:
+            size (int): The size of the new square
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Method that returns the area of the square """
+        return (self.__size * self.__size)
