de893bd1027f8ac8f3ef3dca68038b8aef20ec41
AmilcarArmmand <armmand@gmail.com>
2021-01-06 04:50:49 +0000

    Fixed printing of the position of the square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,5 +57,8 @@ class Square:
         if self.__size == 0:
             print("")
 
-        for i in range(self.__size):
-            print("#" * self.__size)
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(' ', end='') for i in range(0, self.__position[0])]
+            [print('#', end='') for i in range(0, self.__size)]
+            print("")
