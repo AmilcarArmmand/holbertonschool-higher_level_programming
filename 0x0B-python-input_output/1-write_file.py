7d5fcb9617fe4f97e2583f708a83eaf2f6557f7e
AmilcarArmmand <armmand@gmail.com>
2021-01-20 23:35:23 +0000

    Changed line 8 to return line, code was duplicated in line 9

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """ Write to a file in UTF-8 format return characters written """
     with open(filename, 'w', encoding='utf-8') as myfile:
-        myfile.write(text)
         return myfile.write(text)
