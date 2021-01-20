918ce07d4eb1ecb4382f799e33ef0cd2e8dcab4f
AmilcarArmmand <armmand@gmail.com>
2021-01-20 02:50:52 +0000

    Closed the comments in line 2 with triple quotes

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-""" Check for instance of the object
+""" Check for instance of the object """
 
 
-"""
 def is_kind_of_class(obj, a_class):
     """ Return True if same instance of class or False if inherited class """
     return (isinstance(obj, a_class))
