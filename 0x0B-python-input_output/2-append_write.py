fe96e0605eb7349b6c83a237b67dfdb3746b6a0a
AmilcarArmmand <armmand@gmail.com>
2021-01-20 23:48:54 +0000

    Changed write permission to 'a+'

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""   """
+""" Function that appends a string at the end of a text file """
 
 
 def append_write(filename="", text=""):
-    """   """
-    with open(filename, 'a', encoding='utf-8') as myfile:
+    """ Returns number of characters appended to the end of file """
+    with open(filename, 'a+', encoding='utf-8') as myfile:
         return myfile.write(text)
