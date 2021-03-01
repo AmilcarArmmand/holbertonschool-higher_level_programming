52ad419992ddb82daf4d0be052b7e54634ad88e9
AmilcarArmmand <armmand@gmail.com>
2021-03-01 20:07:41 +0000

    updated comments

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
--- script that creates a table second_table in the database in
--- MySQL server, database name will be passed as argument of mysql command
+-- script that creates a table second_table in the database in a MySQL server
+-- database name will be passed as argument of mysql command
+-- without using the SELECT and SHOW statements
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, "John", 10),
