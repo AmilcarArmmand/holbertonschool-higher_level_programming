720ecff0c0730c5469c90c95f774ff146a0abcaa
AmilcarArmmand <armmand@gmail.com>
2021-03-01 20:16:05 +0000

    updated comments

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table second_table of the database
--- MySQL server, results will display the score and name in order by score
+-- MySQL server with a score >= 10 in order by score
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
