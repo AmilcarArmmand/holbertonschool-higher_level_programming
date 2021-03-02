ae55f49b155a60ba733ff43dceaa2b58026d3357
AmilcarArmmand <armmand@gmail.com>
2021-03-02 21:38:04 +0000

    removed whitespace between database_name and table_name delimiter

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- creates user user_0d_1 on server in localhost and sets password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- all privileges granted
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
