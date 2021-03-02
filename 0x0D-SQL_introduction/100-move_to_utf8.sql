3a137c84b2b8be99d3601244117a3f170b726f59
AmilcarArmmand <armmand@gmail.com>
2021-03-02 04:04:03 +0000

    Fixed database not set error with USE command

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,9 @@
 -- script that converts database to UTF8
 -- convert the database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- select database to use for this session of commands
+USE hbtn_0c_0;
 -- convert the table
 ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- convert the Field name in first_table
-ALTER TABLE first_table CONVERT COLUMN name name TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE COLUMN name name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
