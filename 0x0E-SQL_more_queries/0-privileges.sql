441f500f677f0834c3a3d1e7656f98435676085a
AmilcarArmmand <armmand@gmail.com>
2021-03-02 21:04:39 +0000

    added task 0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- MySQL script that lists all privileges for users on server in localhost
+-- user user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- user user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
