358b9a71a2488c01fdcc62174c47ef4a2d9f5475
AmilcarArmmand <armmand@gmail.com>
2021-03-03 03:30:31 +0000

    added task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,11 @@
+-- MySQL script that lists all shows in the database hbtn_0d_tvshows
+-- display without a genre linked: tv_shows.title - tv_show_genres.genre_id
+-- Sort results in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- Use only one SELECT statement
+-- USE `hbtn_0d_tvshows`;
+SELECT s.title, g.genre_id FROM tv_shows AS s
+       LEFT JOIN tv_show_genres AS g
+       ON s.id = g.show_id
+       WHERE g.show_id IS NULL
+       ORDER BY s.title, g.genre_id ASC;
