92655eaf9a65f871a6466925048baef3c877da7b
AmilcarArmmand <armmand@gmail.com>
2021-03-03 02:09:14 +0000

    added task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- MySQL script that lists all cities of California in the database hbtn_0d_usa
+-- USE `hbtn_0d_usa`;
+SELECT id, name FROM cities
+       WHERE state_id IN(
+       SELECT id
+       FROM states
+       WHERE name = 'California')
+       ORDER BY id ASC;
