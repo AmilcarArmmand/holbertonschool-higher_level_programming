f94415849463e8a1be756291da9f65e14f8e1dcc
AmilcarArmmand <armmand@gmail.com>
2021-03-04 01:59:26 +0000

    added task 19

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,8 @@
--- script that lists all rows of the table first_table
+-- script that displays the top 3 of cities temperature
+-- during July and August ordered by temperature (descending)
 -- from the MySQL database hbtn_0c_0 in MySQL server
-SELECT * FROM temperatures;
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+       WHERE month=7 OR month=8
+       GROUP By city
+       ORDER BY avg_temp DESC
+       LIMIT 3;
