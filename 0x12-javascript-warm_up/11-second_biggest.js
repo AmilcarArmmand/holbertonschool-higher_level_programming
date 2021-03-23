55e8913731f024d4ae6235b866d62475e27e8550
AmilcarArmmand <armmand@gmail.com>
2021-03-23 03:50:41 +0000

    updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,9 @@
 /* a script that searches the second biggest integer in the list of arguments */
 'use strict';
 let x = process.argv.slice(2);
+x = x.map(y => parseInt(y));
+x = new Set(x);
+x = Array.from(x);
 x.sort();
 x.reverse();
 if (x.length < 2) {
