5ed5c8379ef0cd3f614b41d2cb57a27f37f8beda
AmilcarArmmand <armmand@gmail.com>
2021-03-25 00:11:00 +0000

    refactored with sorting of numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 /* a script that searches the second biggest integer in the list of arguments */
 'use strict';
-let x = process.argv.slice(2);
-x = x.map(y => parseInt(y));
-x = new Set(x);
-x = Array.from(x);
-x.sort();
-x.reverse();
-if (x.length < 2) {
+const numbers = process.argv;
+if (process.argv.length < 4) {
   console.log(0);
 } else {
-  console.log(x[1]);
+  numbers.sort(function (a, b) {
+    return b - a;
+  });
+  console.log(numbers[3]);
 }
