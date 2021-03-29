216f92dbbe96d754d4e4676738d526aaaf67d129
AmilcarArmmand <armmand@gmail.com>
2021-03-29 17:08:11 +0000

    made pep8 compliant, set filter to variable to shorten line

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engin)
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    s = '%a%'
+    states = session.query(State).filter(State.name.like(s)).order_by(State.id)
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
