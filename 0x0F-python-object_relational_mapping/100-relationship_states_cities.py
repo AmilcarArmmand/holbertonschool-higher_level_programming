c268aec6616abe0442434e3827db46d2d8d85b1c
AmilcarArmmand <armmand@gmail.com>
2021-04-03 02:32:30 +0000

    updated

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,7 +12,8 @@ from relationship_city import City
 if __name__ == "__main__":
     engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                       argv[2],
-                                                                      argv[3]))
+                                                                      argv[3]),
+                          pool_pre_ping=True)
 
     Base.metadata.create_all(engin)
 
