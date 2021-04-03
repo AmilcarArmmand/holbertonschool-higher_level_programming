0ab178bb2f90276750c867de5b9d6ce4072c9aab
AmilcarArmmand <armmand@gmail.com>
2021-04-03 02:11:48 +0000

    added task 15 files

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -6,8 +6,8 @@ script that changes the name of a State object from the database hbtn_0e_6_usa
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == "__main__":
     engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
@@ -17,11 +17,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engin)
     Session = sessionmaker(bind=engin)
     session = Session()
-
-    rows = session.query(City, State).filter(City.state_id == State.id)\
-                                     .order_by(City.id).all()
-
-    for city, state in rows:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-
+    my_state = State(name="California")
+    my_state.my_city = [City(name="San Francisco")]
+    session.add(my_state)
+    session.commit()
     session.close()
