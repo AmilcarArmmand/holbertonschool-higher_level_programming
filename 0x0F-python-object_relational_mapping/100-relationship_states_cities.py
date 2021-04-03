7575c02cc97ecbf74fbc3b0c03b5b7e14a35f8ae
AmilcarArmmand <armmand@gmail.com>
2021-04-03 02:47:47 +0000

    added sys to arguments

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,9 +10,9 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-    engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                      argv[2],
-                                                                      argv[3]),
+    """Session engine"""
+    engin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                          format(sys.argv[1], sys.argv[2], sys.argv[3]),
                           pool_pre_ping=True)
 
     Base.metadata.create_all(engin)
