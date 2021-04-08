a563c4ef40a55a872e9a12a024eab5e19c563257
AmilcarArmmand <armmand@gmail.com>
2021-04-08 20:48:37 +0000

    refactored to be 3 lines only

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
-
+# script that takes in a URL and displays the size of the body of the response
 curl -o /dev/null -sw %"{size_download}\n" "$1"
