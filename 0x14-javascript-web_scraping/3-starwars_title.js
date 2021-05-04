acc7fd12f7fcc9a7a08987987b161ec01bfbaa1d
AmilcarArmmand <armmand@gmail.com>
2021-05-04 18:15:15 +0000

    changed variable names for clarity

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,9 +3,9 @@
 /* Star Wars movie where the episode number matches a given integer. */
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
-const title = process.argv[2];
+const movie_number = process.argv[2];
 
-request(url + title, function (error, response, body) {
+request(url + movie_number, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
