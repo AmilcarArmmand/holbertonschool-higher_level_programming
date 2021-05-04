926cb60cb7b232c59f56d3ebde7ce8da874e65ef
AmilcarArmmand <armmand@gmail.com>
2021-05-04 18:16:52 +0000

    changed variable names to camel-case

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,9 +3,9 @@
 /* Star Wars movie where the episode number matches a given integer. */
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
-const movie_number = process.argv[2];
+const movieNumber = process.argv[2];
 
-request(url + movie_number, function (error, response, body) {
+request(url + movieNumber, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
