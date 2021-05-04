cd8ed1eb158663cbb875b4f4499d6cea64ed70b6
AmilcarArmmand <armmand@gmail.com>
2021-05-04 18:53:56 +0000

    added task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* using the Star Wars API https://swapi-api.hbtn.io/ prints the title of the */
+/* Star Wars movie where the episode number matches a given integer. */
+const request = require('request');
+const url = process.argv[2]
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const results = JSON.parse(body).results;
+    const appears = results.filter((x) => x.characters.filter((y) => y.match(/people\/18\//)).length !== 0);
+    console.log(appears.length);
+  }
+});
