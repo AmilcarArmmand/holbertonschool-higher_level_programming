f3de84894573e7885f1ad42258a2a0dc6713a28a
AmilcarArmmand <armmand@gmail.com>
2021-05-05 02:39:48 +0000

    added async/await Promise response

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
-/* a script that prints all characters of a Star Wars movie in the same order in /films/ */
+/* a script that prints all characters of a Star Wars movie in the same order at /films/ response */
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async function (error, response, body) {
   if (error) {
     console.log(error);
   }
   for (const x of JSON.parse(body).characters) {
-    request(x, function (error, response, body) {
-      if (error) {
-        console.log(error);
-      }
-      console.log(JSON.parse(body).name);
+    await new Promise(function (resolve, reject) {
+      request(x, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
   }
 });
