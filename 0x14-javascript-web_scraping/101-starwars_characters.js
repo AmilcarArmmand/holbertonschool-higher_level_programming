454f18e02158f78d1de7abb26ddb44a570cd91dd
AmilcarArmmand <armmand@gmail.com>
2021-05-05 14:51:22 +0000

    updated var names

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,14 +6,14 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async function
   if (error) {
     console.log(error);
   }
-  for (const x of JSON.parse(body).characters) {
+  for (const person of JSON.parse(body).characters) {
     await new Promise(function (resolve, reject) {
-      request(x, function (error, response, body) {
+      request(person, function (error, response, body) {
         if (error) {
           console.log(error);
         }
         console.log(JSON.parse(body).name);
-        resolve();
+        resolve(body);
       });
     });
   }
